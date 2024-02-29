530bc85472ff87cb4098a5356087ccd058f29836
hasibuddin360 <hasibuddin404@gmail.com>
2024-02-29 15:58:25 +0600

    ready to launch

--- app.js
+++ app.js
@@ -2,24 +2,27 @@
 
 let userScore = 0;
 let compScore = 0;
+let drawCount = 0;
 
 const choices = document.querySelectorAll(".choice");
 const msg = document.querySelector("#msg");
 
-const userSocrePera = document.querySelector("#user-score")
-const compSocrePera = document.querySelector("#comp-score")
+const userSocrePera = document.querySelector("#user-score");
+const compSocrePera = document.querySelector("#comp-score");
 
 const genCompChoice = () => {
   //This is Funtion
   const optinos = ["stone", "paper", "scissors"]; //This is Array
   const randIdx = Math.floor(Math.random() * 3);
+  console.log(randIdx);
   return optinos[randIdx];
 };
 
 const drawGame = () => {
-  console.log("Game was draw.");
   msg.innerText = "Game was draw. Play again!";
-  msg.style.backgroundColor = "#081b31"
+  msg.style.backgroundColor = "#081b31";
+  drawCount++;
+  document.getElementById("draw-score").innerText = drawCount;
 };
 
 const shoWinner = (userWine, userChoice, CompChoice) => {
@@ -27,25 +30,21 @@ const shoWinner = (userWine, userChoice, CompChoice) => {
     userScore++;
     userSocrePera.innerText = userScore;
     msg.innerText = `You Win! Your ${userChoice} beats ${CompChoice}`;
-    msg.style.backgroundColor = "green"
-    msg.style.borderRadius = "25px"
-    msg.style.padding = "20px"
-    
+    msg.style.backgroundColor = "green";
+    msg.style.borderRadius = "25px";
+    msg.style.padding = "20px";
   } else {
     compScore++;
     compSocrePera.innerText = compScore;
     msg.innerText = `You lose! ${CompChoice} beats Your ${userChoice}`;
-    msg.style.backgroundColor = "red"
-    msg.style.borderRadius = "25px"
-    msg.style.padding = "20px"
+    msg.style.backgroundColor = "red";
+    msg.style.borderRadius = "25px";
+    msg.style.padding = "20px";
   }
 };
 
 const playGame = (userChoice) => {
-  console.log("user choice =", userChoice);
   const CompChoice = genCompChoice();
-  console.log("comp choice =", CompChoice);
-
   if (userChoice === CompChoice) {
     // Draw game
     drawGame();
@@ -63,7 +62,6 @@ const playGame = (userChoice) => {
 };
 
 choices.forEach((choice) => {
-  console.log(choice);
   choice.addEventListener("click", () => {
     const userChoice = choice.getAttribute("id");
     playGame(userChoice);
